505da0263f946746a758b97fce1cf4c481b394b7
dominic19950228 <57146755+dominic19950228@users.noreply.github.com>
2024-03-22 16:32:28 +1100

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let gameOver = false;
 
 // 初始化牌堆
 function initDeck() {
-  const suits = ['?', '?', '?', '?'];
+  const suits = ['♣', '♦', '♥', '♠'];
   const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
   for (let suit of suits) {
     for (let value of values) {
